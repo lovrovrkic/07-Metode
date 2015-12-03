06a099de24836266c83661f404d89e5e9e05d345
Lovro Vrkic <lovrovr@hotmail.com>
2015-12-03 20:45:29 +0100

    ZamijeniGenericki solved

--- ZamijeniGenericki/ZamijeniGenerički.cs
+++ ZamijeniGenericki/ZamijeniGenerički.cs
@@ -8,7 +8,7 @@ namespace Vsite.CSharp
     class Program
     {
         // Primjer kako prevoditelj automatski generira različite metode za različite tipove parametara
-        static void ZamijeniGenerički<T>(T o1, T o2)
+        static void ZamijeniGenerički<T>(ref T o1, ref T o2)
         {
             T temp = o2;
             o2 = o1;
@@ -17,18 +17,18 @@ namespace Vsite.CSharp
 
         static void Main(string[] args)
         {
-            // TODO: prevesti program i otvoriti i pogledati generirani kod s ILDASM alatom te međukod metode Main prekopirati u ZamijeniGenerički.txt
-            // TODO: u datoteku odgovoriti: koliko se metoda iz metode Main poziva?
+            // prevesti program i otvoriti i pogledati generirani kod s ILDASM alatom te međukod metode Main prekopirati u ZamijeniGenerički.txt
+            // u datoteku odgovoriti: koliko se metoda iz metode Main poziva?
 
             int i1 = 5;
             int i2 = 10;
-            ZamijeniGenerički(i1, i2);
+            ZamijeniGenerički(ref i1, ref i2);
 
             double d1 = 3.14;
             double d2 = 2.76;
-            ZamijeniGenerički(d1, d2);
+            ZamijeniGenerički(ref d1, ref d2);
 
-            ZamijeniGenerički("mirko", "slavko");
+            //ZamijeniGenerički("mirko", "slavko");
 
         }
     }
